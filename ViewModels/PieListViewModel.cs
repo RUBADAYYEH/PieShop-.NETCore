1ab3ae61c75a622605094861c39fd950f763a533
ruba dayy <ruba.dayyeh@gmail.com>
2024-07-13 16:25:32 +0300

    Adding Views :

---
+++ ViewModels/PieListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PieShop.Models;
+
+namespace PieShop.ViewModels
+{
+	public class PieListViewModel
+	{
+		public string? CurrentCategory { get;} 
+		public IEnumerable<Pie> Pies {  get;}
+		public PieListViewModel(IEnumerable<Pie> pies,string? currentCategory)
+		{
+			Pies = pies;
+			CurrentCategory = currentCategory;
+		}
+	}
+}
