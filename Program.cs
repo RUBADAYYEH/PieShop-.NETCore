43b10580674793ab055122545e20907df9ad554e
ruba dayy <ruba.dayyeh@gmail.com>
2024-07-13 13:08:34 +0300

    Adding Models classes for Pie and Category

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using PieShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+app.UseStaticFiles();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.MapDefaultControllerRoute();
+
+app.Run();
